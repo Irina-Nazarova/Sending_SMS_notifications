933f481bd7cf3d2147a37c02aee8c090ba34fc13
Irina <irina.g.nazarova@mail.ru>
2020-09-09 18:52:31 +0300

    done

--- homework.py
+++ homework.py
@@ -6,15 +6,26 @@ from twilio.rest import Client
 
 def get_status(user_id):
     params = {
-        ...
+        "user_ids": user_id,
+        "v": 5.92,
+        "access_token": 'VK_TOKEN',
+        "fields": "online"
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    users_list = requests.post("https://api.vk.com/method/users.get", params=params)
+    return users_list.json()["response"][0]["online"]
 
 
 def sms_sender(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    account_sid = 'TWILIO_ACCOUNT_SID'
+    auth_token = 'TWILIO_AUTH_TOKEN'
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+                    .create(
+                        body="sms_text",
+                        from_='NUMBER_FROM',
+                        to='NUMBER_TO'
+    )
+    return message.sid
 
 
 if __name__ == "__main__":
